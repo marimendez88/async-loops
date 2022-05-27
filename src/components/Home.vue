4568ddb4249f33cbd601df3c0a2dcd2d2c4f76e5
Maria Jose Mendez <majomendez88mena@gmail.com>
2022-05-27 11:40:04 -0600

    feat(loops): ready tyo analizy

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,17 +6,20 @@
 <section class="loops">
   <section class="for-loop">
     <h2>For Loop: {{forItems.length}}</h2>
+    <h4>Items processed: <b class="red">{{forItemsProcessed}}</b> vs Items Resolved:  <b class="red">{{forItemsResolved}}</b></h4>
     <h5>Execution Time : {{forTimer}} milliseconds</h5>
     <h5>How many times United States appears : {{forTotalAppears}}</h5>
     <button class="btn-primary" @click="forCountCountry">Calculate</button>
     <table id="results">
         <tr>
+            <th>Processed</th>
      <th>City</th>
       <th>State</th>
     <th>Country</th>
     <th>Time</th>
   </tr>
    <tr  v-for="item in forItems">
+    <td class="red">{{item.ready}}</td>
     <td>{{item.city}}</td>
     <td>{{item.state}}</td>
     <td>{{item.country}}</td>
@@ -26,41 +29,47 @@
   </section>
   <section class="forEach-loop">
     <h2>ForEach Loop {{forEachItems.length}}</h2>
+       <h4>Items processed: <b class="green">{{forEachItemsProcessed}}</b> vs Items Resolved:  <b class="green">{{forEachItemsResolved}}</b></h4>
      <h5>Execution Time : {{forEachTimer}} milliseconds</h5>
     <h5>How many times United States appears : {{forEachTotalAppears}}</h5>
     <button class="btn-primary" @click="forEachCountCountry">Calculate</button>
       <table id="results">
         <tr>
+            <th>Processed</th>
      <th>City</th>
       <th>State</th>
     <th>Country</th>
       <th>Time</th>
   </tr>
    <tr  v-for="item in forEachItems">
+    <td class="green">{{item.ready}}</td>
     <td>{{item.city}}</td>
     <td>{{item.state}}</td>
     <td>{{item.country}}</td>
-    <td class="red">{{item.valueAdded}}</td>
+    <td class="green">{{item.valueAdded}}</td>
   </tr>
     </table>
   </section>
   <section class="map-loop">
     <h2>Map Loop {{mapItems.length}}</h2>
+    <h4>Items processed: <b class="blue">{{mapItemsProcessed}}</b> vs Items Resolved:  <b class="blue">{{mapItemsResolved}}</b></h4>
      <h5>Execution Time : {{mapTimer}} milliseconds</h5>
     <h5>How many times United States appears : {{mapTotalAppears}}</h5>
     <button class="btn-primary" @click="mapCountCountry">Calculate</button>
       <table id="results">
         <tr>
+            <th>Processed</th>
      <th>City</th>
       <th>State</th>
     <th>Country</th>
       <th>Time</th>
   </tr>
    <tr  v-for="item in mapItems">
+    <td class="blue">{{item.ready}}</td>
     <td>{{item.city}}</td>
     <td>{{item.state}}</td>
     <td>{{item.country}}</td>
-    <td class="red">{{item.valueAdded}}</td>
+    <td class="blue">{{item.valueAdded}}</td>
   </tr>
     </table>
   </section>
@@ -82,22 +91,31 @@ export default defineComponent({
       state: string;
       country: string;
       valueAdded: string;
+      ready: string
     };
     let searchTerm = ref('paris');
 
  const _axios = new AxiosService();
     _axios.getAxiosCall(searchTerm.value).then(res => {
        forItems.value = res.data
+    });
+    _axios.getAxiosCall(searchTerm.value).then(res => {
        mapItems.value = res.data
+    });
+    _axios.getAxiosCall(searchTerm.value).then(res => {
        forEachItems.value = res.data
     });
     const searchOnApi = () => {
         resetValues();
-       _axios.getAxiosCall(searchTerm.value).then(res => {
-        forItems.value = res.data
-        mapItems.value = res.data
-        forEachItems.value = res.data
-      });
+        _axios.getAxiosCall(searchTerm.value).then(res => {
+       forItems.value = res.data
+    });
+    _axios.getAxiosCall(searchTerm.value).then(res => {
+       mapItems.value = res.data
+    });
+    _axios.getAxiosCall(searchTerm.value).then(res => {
+       forEachItems.value = res.data
+    });
     }
     const resetValues = () => {
       forTimer.value = 0;
@@ -112,67 +130,100 @@ export default defineComponent({
     let forItems: Ref<Item[]> = ref([])
     let forTimer: Ref<number> = ref(0);
     let forTotalAppears: Ref<number> = ref(0);
+    let forItemsProcessed: Ref<number> = ref(0);
+    let forItemsResolved: Ref<number> = ref(0);
     const forCountCountry = async () => {
        forTotalAppears.value = 0;
+       forItemsProcessed.value = 0;
+       forItemsResolved.value = 0;
       const initialTime = performance.now();
       const logger = getLogger("For Loop");
       for (let item of forItems.value) {
+        forItemsProcessed.value++;
         const start = Date.now();
-         await waitFor(0.5);
+        await waitFor(1,item);
         if (item.country === 'United States') {
           forTotalAppears.value++;
         }
         const later = Date.now();
         const total = later - start;
+        item.valueAdded = (Math.round(total/1000)).toString();
         logger(item.city, total);
-        item.valueAdded = total.toString();
+        forItemsResolved.value++;
       }
       const finalTime = performance.now();
-      forTimer.value = seconds;
+      forTimer.value = finalTime - initialTime;
     }
 
-    // * Map variables
-    let mapItems: Ref<Item[]> = ref([])
-    let mapTimer: Ref<number> = ref(0);
-    let mapTotalAppears: Ref<number> = ref(0);
-     const mapCountCountry = async () => {
-       mapTotalAppears.value = 0;
-      const initialTime = performance.now();
-      mapItems.value.map(item => {
-        if (item.country === 'United States') {
-          mapTotalAppears.value++;
-        }
-      });
-      const finalTime = performance.now();
-      mapTimer.value = finalTime - initialTime;
-    }
 
     // * ForEach variables
     let forEachItems: Ref<Item[]> = ref([])
     let forEachTimer: Ref<number> = ref(0);
     let forEachTotalAppears: Ref<number> = ref(0);
-
+    let forEachItemsProcessed: Ref<number> = ref(0);
+    let forEachItemsResolved: Ref<number> = ref(0);
      const forEachCountCountry = async () => {
        forEachTotalAppears.value = 0;
+       forEachItemsProcessed.value = 0;
+        forEachItemsResolved.value = 0;
       const initialTime = performance.now();
-        forItems.value.forEach(async item => {
-        await waitFor(2);
+        const logger = getLogger("ForEach Loop");
+        forEachItems.value.forEach(async item => {
+         const start = Date.now();
+        forEachItemsProcessed.value++;
+        await waitFor(1, item);
         if (item.country === 'United States') {
           forEachTotalAppears.value++;
         }
+        const later = Date.now();
+        const total = later - start;
+         item.valueAdded = (Math.round(total/1000)).toString();
+        logger(item.city + item.state, total);
+        forEachItemsResolved.value++;
       });
       const finalTime = performance.now();
       forEachTimer.value = finalTime - initialTime;
     }
+        // * Map variables
+    let mapItems: Ref<Item[]> = ref([])
+    let mapTimer: Ref<number> = ref(0);
+    let mapTotalAppears: Ref<number> = ref(0);
+    let mapItemsProcessed: Ref<number> = ref(0);
+    let mapItemsResolved: Ref<number> = ref(0);
+
+     const mapCountCountry = async () => {
+        mapTotalAppears.value = 0;
+        mapItemsProcessed.value = 0;
+        mapItemsResolved.value = 0;
+        const initialTime = performance.now();
+        const logger = getLogger("Map Loop");
+        mapItems.value.map(async item => {
+        const start = Date.now();
+        mapItemsProcessed.value++;
+        await waitFor(1, item);
+        if (item.country === 'United States') {
+          mapTotalAppears.value++;
+        }
+        const later = Date.now();
+        const total = later - start;
+         item.valueAdded = (Math.round(total/1000)).toString();
+        logger(item.city + item.state, total);
+        mapItemsResolved.value++;
+      });
+      const finalTime = performance.now();
+      mapTimer.value = finalTime - initialTime;
+    }
 
 
 
-    // simulates an async flow, a network request for example
-    const  waitFor = (seconds: number)  => {
-    // the fake asynchronous task
-    return new Promise((resolve, reject) => {
-      setTimeout(resolve, seconds * 1000);
-     });
+    const  waitFor = (seconds: number, item: Item)  => {
+      // the fake asynchronous task
+      return new Promise<void>(resolve => {
+        item.ready = 'Yes';
+        setTimeout(() => {
+          resolve();
+        }, seconds * 1000);
+      });
     }
 
      // creates a logger function to print logs with function name
@@ -193,14 +244,20 @@ export default defineComponent({
       forItems,
       forTimer,
       forTotalAppears,
+      forItemsProcessed,
+      forItemsResolved,
       forCountCountry,
       mapItems,
       mapTimer,
       mapTotalAppears,
+      mapItemsProcessed,
+      mapItemsResolved,
       mapCountCountry,
       forEachItems,
       forEachTimer,
       forEachTotalAppears,
+      forEachItemsProcessed,
+      forEachItemsResolved,
       forEachCountCountry,
       searchOnApi,
     }
@@ -255,6 +312,12 @@ export default defineComponent({
   }
   .red {
     color:red;
+  }
+  .green {
+    color:green;
+  }
+  .blue {
+    color:blue;
   }
   #results {
   font-family: Arial, Helvetica, sans-serif;
